df85ef0c7243bfedf3d0df908ddec8b1e62d29f4
Jared Barboza <jared.m.barboza@gmail.com>
2019-05-09 12:04:20 -0500

    update powershell to use concfg

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,6 @@
+$console = $host.UI.RawUI
+$concfg = 'D:\Dropbox\bin\tools\concfg\bin\concfg'
+
 # RUBY STUFF
 #####################
 
@@ -11,17 +14,9 @@ $env:LANG = "en_US.UTF-8"
 # COLORS
 #####################
 
-$console = $host.UI.RawUI
-$console.BackgroundColor = "black"
-$console.ForegroundColor = "white"
-$colors = $host.PrivateData
-$colors.VerboseForegroundColor = "white"
-$colors.VerboseBackgroundColor = "blue"
-$colors.WarningForegroundColor = "yellow"
-$colors.WarningBackgroundColor = "darkgreen"
-$colors.ErrorForegroundColor = "white"
-$colors.ErrorBackgroundColor = "red"
-
+function Set-Theme([string]$theme) {
+  . "$concfg" import "$theme"
+}
 
 # WINDOW/BUFFER SIZE
 #####################
@@ -42,12 +37,6 @@ $console.WindowSize = $size
 
 . 'D:\Dropbox\bin\tools\posh-git\profile.example.ps1'
 
-# LOAD PLUGINS
-#####################
-
-$scripts_path = $PSScriptRoot + "\powershell\"
-Get-ChildItem ( $scripts_path + "*.ps1") | ForEach-Object {. (Join-Path $scripts_path $_.Name) }
-
 # set git\bin into the ENVIRONMENT
 #####################
 
@@ -66,6 +55,18 @@ function prompt {
 # ALIASES
 #####################
 
+function Rename-GitTag([string] $old, [string] $new) {
+    git tag $new $old
+    git tag -d $old
+    git push origin :refs/tags/$old
+    git push --tags
+}
+
+function took() {
+  $command = Get-History -Count 1
+  $command.EndExecutionTime - $command.StartExecutionTime
+}
+
 # function to grep history
 function grepHistory ([string] $word) {
   Get-History -c 1000 | where {$_.commandline.contains($word)}
@@ -78,5 +79,10 @@ function grepServices ( [string] $svcName ) {
 Set-Alias find-service grepServices
 Set-Alias i invoke-history
 Set-Alias gh grepHistory
+
+# Posh Theme Chooser
+Import-Module oh-my-posh
+
+Set-Theme Sorin
 
 cd ~
